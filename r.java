f425dcf4ddc605b9adbae2c5c82495a75fba3341
sangari96 <shivashankari643@gmail.com>
2016-09-08 18:17:02 +0530

    Create r.java

---
+++ r.java
@@ -0,0 +1,15 @@
+import java.io.*
+class r
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a>0)
+System.out.println("no is positive");
+else if(a<0)
+System.out.println("no is negatine");
+else
+System.out.println(0);
+}
+}
